114b45d552b7bc2cff7a6dc27074873230b06a6d
Phan Nam Khánh <khanhnhim21102004@gmail.com>
2026-08-24 23:18:24 +0700

    update about me

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Khanh";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["AI Research", "Computer Vision", "Mobile App Development", "YOLO Models", "Flutter Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
