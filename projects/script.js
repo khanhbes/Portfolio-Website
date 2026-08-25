5996a5cb05320bf8f83208447c312ee83c5a428a
Phan Nam Khánh <khanhnhim21102004@gmail.com>
2026-08-25 11:45:48 +0700

    update chat

--- projects/script.js
+++ projects/script.js
@@ -110,7 +110,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/6a8d1845dcccab344b66d6eb/1k0ridk4g';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
